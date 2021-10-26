c40c96da3c3aa146c984b40629ee0284876ee96e
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-10-26 21:09:52 +0900

    Write getAction Method

--- WEB09_MemberMGT_Model2/src/main/java/com/ezen/controller/ActionFactory.java
+++ WEB09_MemberMGT_Model2/src/main/java/com/ezen/controller/ActionFactory.java
@@ -1,7 +1,24 @@
 package com.ezen.controller;
 
+import com.ezen.controller.action.Action;
+import com.ezen.controller.action.JoinAction;
+import com.ezen.controller.action.JoinFormAction;
+import com.ezen.controller.action.LoginAction;
+import com.ezen.controller.action.LoginFormAction;
+
 public class ActionFactory {
 	private ActionFactory() {}
 	private static ActionFactory ist = new ActionFactory();
 	public static ActionFactory getInstance() {return ist;}
+	
+	public Action getAction(String command) {
+		Action ac = null;
+		
+		if(command.equals("loginForm")) ac = new LoginFormAction(); else
+		if(command.equals("login")) ac = new LoginAction(); else
+		if(command.equals("joinForm")) ac = new JoinFormAction(); else
+		if(command.equals("join")) ac = new JoinAction();
+		
+		return ac;
+	}
 }
