288101089d45df101b3967a6a454a009652f2b2e
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-10-26 21:01:09 +0900

    Create LoginAction.java

---
+++ WEB09_MemberMGT_Model2/src/main/java/com/ezen/controller/action/LoginAction.java
@@ -0,0 +1,16 @@
+package com.ezen.controller.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginAction implements Action {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 전달 받은 로그인 정보로 유효 로그인 동작에 관한 검증
+		
+		// main.jsp로 이동
+	}
+
+}
