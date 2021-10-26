ea0e9cae43857298efd8854b5465ead73230b322
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-10-26 20:44:50 +0900

    Write doGet Method

--- WEB09_MemberMGT_Model2/src/main/java/com/ezen/controller/MemberServlet.java
+++ WEB09_MemberMGT_Model2/src/main/java/com/ezen/controller/MemberServlet.java
@@ -1,11 +1,15 @@
 package com.ezen.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ezen.controller.action.Action;
+import com.ezen.controller.action.LoginFormAction;
 
 /**
  * Servlet implementation class MemberServlet
@@ -27,6 +31,12 @@ public class MemberServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String command = request.getParameter("command");
+		
+		Action ac = null;
+		if(command.equals("loginForm"))
+			ac = new LoginFormAction();
+		
+		ac.execute(request, response);
 	}
 
 	/**
