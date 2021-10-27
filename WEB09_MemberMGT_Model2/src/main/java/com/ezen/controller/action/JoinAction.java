53d64925f7957d719060dbc102242e1741746c12
YataNox <rladnwls0523@naver.com>
2021-10-27 17:25:16 +0900

    Write JoinAction.java

--- WEB09_MemberMGT_Model2/src/main/java/com/ezen/controller/action/JoinAction.java
+++ WEB09_MemberMGT_Model2/src/main/java/com/ezen/controller/action/JoinAction.java
@@ -2,14 +2,37 @@ package com.ezen.controller.action;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ezen.dao.MemberDao;
+import com.ezen.dto.MemberDto;
+
 public class JoinAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 전달된 정보로 회원가입
+		MemberDto mdto = new MemberDto();
 		
+		mdto.setName(request.getParameter("name"));
+		mdto.setUserid(request.getParameter("userid"));
+		mdto.setUserpwd(request.getParameter("userpwd"));
+		mdto.setEmail(request.getParameter("email"));
+		mdto.setPhone(request.getParameter("phone"));
+		mdto.setAdmin(Integer.parseInt(request.getParameter("name")));
+		MemberDao mdao = MemberDao.getInstance();
+		
+		int result = mdao.insertMember(mdto);
+		
+		if(result == 1) {
+			request.setAttribute("message", "회원가입 완료. 로그인하세요.");
+		}else {
+			request.setAttribute("message", "회원가입 실패. 관리자에게 문의하세요");
+		}
 
+		RequestDispatcher dp = request.getRequestDispatcher("member/loginForm.jsp");
+		dp.forward(request, response);
 	}
 
 }
