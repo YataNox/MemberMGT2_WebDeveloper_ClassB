c47b7d7354389911b79a0a52ec1675e93f2a6bc0
YataNox <rladnwls0523@naver.com>
2021-10-27 17:33:55 +0900

    Write confirmID, insertMember method

--- WEB09_MemberMGT_Model2/src/main/java/com/ezen/dao/MemberDao.java
+++ WEB09_MemberMGT_Model2/src/main/java/com/ezen/dao/MemberDao.java
@@ -72,4 +72,49 @@ public class MemberDao {
 		return mdto;
 	}
 
+	public int confirmID(String userid){
+		int result = 0;
+		con = getConnection();
+		
+		String sql = "select * from member where userid= ?";
+		try {
+			 pstmt = con.prepareStatement(sql);
+			 pstmt.setString(1, userid);
+			 rs = pstmt.executeQuery();
+			 if(rs.next()) { // 사용 중
+				 result = 1;
+			 }else { // 사용가능
+				 result = -1;
+			 }
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		close();
+		return result;
+	}
+
+	public int insertMember(MemberDto mdto) {
+		int result = 0;
+		
+		con = getConnection();
+		
+		String sql = "insert into member values(?,?,?,?,?,?)";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, mdto.getName());
+			pstmt.setString(2, mdto.getUserid());
+			pstmt.setString(3, mdto.getUserpwd());
+			pstmt.setString(4, mdto.getEmail());
+			pstmt.setString(5, mdto.getPhone());
+			pstmt.setInt(6, mdto.getAdmin());
+			
+			result = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		close();
+		return result;
+	}
 }
